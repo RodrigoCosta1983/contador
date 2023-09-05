bfd862e13141f7608a26643a04a75aefd56d427a
RodrigoCosta1983 <rodrigo25.inf@outlook.com>
2023-09-05 17:47:05 -0300

    UPDT Commit

--- lib/main.dart
+++ lib/main.dart
@@ -15,48 +15,87 @@ class MyApp extends StatelessWidget {
   }
 }
 
-void decrement() {
-  print('Decrement');
-}
-
-void increment() {
-  print('InDecrement');
-}
-
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  void decrement() {
+    print('Decrement');
+  }
+
+  void increment() {
+    print('InDecrement');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.black38,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Pode entrar!',
-            style: TextStyle(
-                fontSize: 26, color: Colors.white, fontWeight: FontWeight.w700),
-          ),
-          Text(
-            '0',
-            style: TextStyle(fontSize: 90, color: Colors.white),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              TextButton(
-                onPressed: decrement,
-                child: Text('Saiu', style: TextStyle(color: Colors.lightBlueAccent),),
-              ),
+      body: Container(
+        decoration:const BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage('https://rodrigocosta-dev.com/images/man.jpg'),
+               fit: BoxFit.cover
+               // image: AssetImage('assets/images/business-online-shopping.jpg')
+          )
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
 
-              TextButton(
-                onPressed: increment,
-                child: Text('Entrou', style: TextStyle(color: Colors.lightBlueAccent),),
+          children: [
+            const Text(
+              'Seja Bem Vindo!!!',
+              style: TextStyle(
+                  fontSize: 36, color: Colors.white, fontWeight: FontWeight.w700),
+            ),
+            const Text(
+              'Pode entrar!',
+              style: TextStyle(
+                  fontSize: 36, color: Colors.white, fontWeight: FontWeight.w700),
+            ),
+            // const SizedBox(height: 32),
+            const Padding(
+              padding: EdgeInsets.all(32),
+              child: Text(
+                '0',
+                style: TextStyle(fontSize: 100, color: Colors.white),
               ),
-            ],
-          ),
-        ],
+            ),
+            // const SizedBox(height: 32),),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                TextButton(
+                  onPressed: decrement,
+                  style: TextButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      fixedSize: const Size(100, 100),
+                      foregroundColor: Colors.black38,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24),
+                      )),
+                  child: const Text(
+                    'Saiu',
+                    style: TextStyle(color: Colors.lightBlueAccent),
+                  ),
+                ),
+                TextButton(
+                  onPressed: increment,
+                  style: TextButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      fixedSize: const Size(100, 100),
+                      foregroundColor: Colors.black38,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24),
+                      )),
+                  child: const Text(
+                    'Entrou',
+                    style: TextStyle(color: Colors.lightBlueAccent),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
